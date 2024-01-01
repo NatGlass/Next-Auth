74d185c19c741c7207cf01484b3fea683672ac54
Visibyte <nathanglass26@gmail.com>
2024-01-01 14:25:12 +0000

    Add signIn functionality and handle authentication errors

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import {signIn} from '@/../auth';
 import {LoginSchema, LoginSchemaType} from '@/schemas';
+import {AuthError} from 'next-auth';
+import {DEFAULT_LOGIN_REDIRECT} from '../../routes';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function login(values: LoginSchemaType) {
@@ -8,6 +11,27 @@ export async function login(values: LoginSchemaType) {
 
   if (!validatedValues.success) {
     return {error: 'Invalid credentials'};
+  }
+
+  const {email, password} = validatedValues.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return {error: 'Invalid credentials'};
+        default:
+          return {error: 'An error occurred'};
+      }
+    }
+
+    throw error;
   }
 
   return {success: 'Verification email sent'};
