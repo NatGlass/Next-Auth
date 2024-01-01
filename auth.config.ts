c4792c77615944ba7aa6e0db36b7cd45ef899327
Visibyte <nathanglass26@gmail.com>
2024-01-01 14:18:27 +0000

    Add credentials provider for authentication

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,33 @@
-import GitHub from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials";
 
-import type { NextAuthConfig } from "next-auth"
+import type { NextAuthConfig } from "next-auth";
+
+import { getUserByEmail } from "@/data/user";
+import { LoginSchema } from "@/schemas";
+import bcrypt from "bcryptjs";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        // To prevent users from sending incorrect credentials directly to the api, it needs to be validated here too
+        const validatedCredentials = LoginSchema.safeParse(credentials);
+
+        if (validatedCredentials.success) {
+          const { email, password } = validatedCredentials.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) {
+            return user;
+          }
+        }
+        return null;
+      }
+    })
+  ],
 } satisfies NextAuthConfig
