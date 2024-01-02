a81696059676d6ac7cadcc9336ac5c7031b1b7a4
Visibyte <nathanglass26@gmail.com>
2024-01-02 17:46:45 +0000

    Add types for extended user and JWT role

---
+++ next-auth.d.ts
@@ -0,0 +1,19 @@
+import "@auth/core/jwt";
+import { type DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & {
+    role: "ADMIN" | "USER"
+}
+
+declare module "next-auth" {
+    interface Session {
+        user: ExtendedUser
+    }
+}
+
+
+declare module "@auth/core/jwt" {
+    interface JWT {
+        role?: "ADMIN" | "USER"
+    }
+}
