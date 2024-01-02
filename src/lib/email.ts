ba4029556ecb4f8a8b013059063d7fae5aef529c
Visibyte <nathanglass26@gmail.com>
2024-01-02 19:35:40 +0000

    Add sendVerificationEmail function to send verification emails

---
+++ src/lib/email.ts
@@ -0,0 +1,15 @@
+import {Resend} from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+// eslint-disable-next-line import/prefer-default-export
+export async function sendVerificationEmail(email: string, token: string) {
+  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+
+  await resend.emails.send({
+    from: 'nat@visibyte.dev',
+    to: email,
+    subject: 'Verify your email address',
+    html: `<a href="${confirmLink}">Confirm email address</a>`,
+  });
+}
