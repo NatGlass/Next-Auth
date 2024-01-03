a8e339e5b7413ea2215ff0874a8bd9f03ca44522
Visibyte <nathanglass26@gmail.com>
2024-01-03 13:27:38 +0000

    Add NewPasswordSchema to index.ts

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -32,3 +32,11 @@ export const ResetSchema = z.object({
 });
 
 export type ResetSchemaType = z.infer<typeof ResetSchema>;
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: 'Please enter a password of at least 6 characters',
+  }),
+});
+
+export type NewPasswordSchemaType = z.infer<typeof NewPasswordSchema>;
