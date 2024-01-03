fca453783dbd26843613cc5c81e014344d069f80
Visibyte <nathanglass26@gmail.com>
2024-01-03 12:40:21 +0000

    Add ResetForm component

---
+++ src/components/auth/reset-form.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+
+import {reset} from '@/actions/reset';
+import {Button} from '@/components/ui/button';
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from '@/components/ui/form';
+import {Input} from '@/components/ui/input';
+import {ResetSchema, ResetSchemaType} from '@/schemas';
+import {useState, useTransition} from 'react';
+import FormError from '../form-error';
+import FormSuccess from '../form-success';
+import CardWrapper from './card-wrapper';
+
+function ResetForm() {
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+  const [isPending, startTransition] = useTransition();
+  const form = useForm<ResetSchemaType>({
+    resolver: zodResolver(ResetSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const onSubmit = (values: ResetSchemaType) => {
+    setError('');
+    setSuccess('');
+
+    startTransition(() => {
+      reset(values).then(data => {
+        if (data.error) {
+          setError(data.error);
+        } else {
+          setSuccess(data.success);
+        }
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Forgotten your password?"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Example@email.com"
+                      type="email"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full" disabled={isPending}>
+            Send reset email
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
+
+export default ResetForm;
