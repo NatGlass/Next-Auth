69af96088cea80a81b03ec147410445681594f7e
Visibyte <nathanglass26@gmail.com>
2024-01-03 19:56:55 +0000

    Refactor settings page component and add transition for updating name

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,14 +1,30 @@
 'use client';
 
+import {settings} from '@/actions/settings';
 import {Button} from '@/components/ui/button';
-import {signOut} from 'next-auth/react';
+import {Card, CardContent, CardHeader} from '@/components/ui/card';
+import {useTransition} from 'react';
 
 function SettingsPage() {
-  const onClick = () => signOut();
+  const [isPending, startTransition] = useTransition();
+  const onClick = () => {
+    startTransition(() => {
+      settings({
+        name: 'Newww',
+      });
+    });
+  };
   return (
-    <div className="shadow-md p-10 rounded-xl">
-      <Button onClick={onClick}>Sign out</Button>
-    </div>
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">Settings</p>
+      </CardHeader>
+      <CardContent>
+        <Button onClick={onClick} disabled={isPending}>
+          Update name
+        </Button>
+      </CardContent>
+    </Card>
   );
 }
 
