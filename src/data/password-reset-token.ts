6dbfc8828f3ebb3b1f5642a1003c6d96055bb060
Visibyte <nathanglass26@gmail.com>
2024-01-03 12:40:45 +0000

    Add password reset token functions

---
+++ src/data/password-reset-token.ts
@@ -0,0 +1,26 @@
+import prisma from '@/lib/db';
+
+export async function getPasswordResetTokenByToken(token: string) {
+  try {
+    const passwordResetToken = await prisma.passwordResetToken.findUnique({
+      where: {token},
+    });
+
+    return passwordResetToken;
+  } catch (error) {
+    return null;
+  }
+}
+
+
+export async function getPasswordResetTokenByEmail(email: string) {
+  try {
+    const passwordResetToken = await prisma.passwordResetToken.findFirst({
+      where: {email},
+    });
+
+    return passwordResetToken;
+  } catch (error) {
+    return null;
+  }
+}
