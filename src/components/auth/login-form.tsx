492ba088d3611ffea6e400cb2dd7ff862bcb6fc7
Visibyte <nathanglass26@gmail.com>
2024-01-03 16:11:17 +0000

    Refactor login form component

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import {zodResolver} from '@hookform/resolvers/zod';
-import {useForm} from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
-import {login} from '@/actions/login';
-import {Button} from '@/components/ui/button';
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from '@/components/ui/form';
-import {Input} from '@/components/ui/input';
-import {LoginSchema, LoginSchemaType} from '@/schemas';
+import { login } from '@/actions/login';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { LoginSchema, LoginSchemaType } from '@/schemas';
 import Link from 'next/link';
-import {useSearchParams} from 'next/navigation';
-import {useState, useTransition} from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useState, useTransition } from 'react';
 import FormError from '../form-error';
 import FormSuccess from '../form-success';
 import CardWrapper from './card-wrapper';
@@ -21,6 +21,7 @@ function LoginForm() {
     searchParams.get('error') === 'OAuthAccountNotLinked'
       ? 'Please login with the auth provider you used to sign up with.'
       : '';
+  const [showTwoFactor, setShowTwoFactor] = useState<boolean>(false);
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
   const [isPending, startTransition] = useTransition();
@@ -37,13 +38,23 @@ function LoginForm() {
     setSuccess('');
 
     startTransition(() => {
-      login(values).then(data => {
-        if (data.error) {
-          setError(data.error);
-        } else {
-          setSuccess(data.success);
-        }
-      });
+      login(values)
+        .then(data => {
+          if (data.error) {
+            form.reset();
+            setError(data.error);
+          }
+
+          if (data.success) {
+            form.reset();
+            setSuccess(data.success);
+          }
+
+          if (data.twoFactor) {
+            setShowTwoFactor(true);
+          }
+        })
+        .catch(() => setError('Something went wrong, please try again.'));
     });
   };
 
@@ -57,41 +68,69 @@ function LoginForm() {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder="Example@email.com"
-                      type="email"
-                      disabled={isPending}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input {...field} placeholder="******" type="password" disabled={isPending} />
-                  </FormControl>
+            {showTwoFactor && (
+              <FormField
+                control={form.control}
+                name="code"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Two Factor Code</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="123456"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            {!showTwoFactor && (
+              <>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Example@email.com"
+                          type="email"
+                          disabled={isPending}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="******"
+                          type="password"
+                          disabled={isPending}
+                        />
+                      </FormControl>
 
-                  <Button variant="link" className="px-0 font-normal" size="sm" asChild>
-                    <Link href="/auth/reset">Forgot password?</Link>
-                  </Button>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+                      <Button variant="link" className="px-0 font-normal" size="sm" asChild>
+                        <Link href="/auth/reset">Forgot password?</Link>
+                      </Button>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
           </div>
           <FormError message={error || urlError} />
           <FormSuccess message={success} />
