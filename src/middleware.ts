b80ef8f1cbe8c0459ed23d25ebfc2c04311c58a5
Visibyte <nathanglass26@gmail.com>
2024-01-03 14:09:18 +0000

    Fix formatting in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 
 import authConfig from '../auth.config';
-import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from '../routes';
+import {DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes} from '../routes';
 
 const {auth} = NextAuth(authConfig);
 
@@ -27,7 +27,7 @@ export default auth(req => {
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL("/auth/login", nextUrl))
+    return Response.redirect(new URL('/auth/login', nextUrl));
   }
 
   return null;
