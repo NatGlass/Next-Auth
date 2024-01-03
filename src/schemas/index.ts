91d1ec794036c2e8a8dca655b4fe95436346127c
Visibyte <nathanglass26@gmail.com>
2024-01-03 19:57:04 +0000

    Fix formatting and add settings schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,4 +1,10 @@
-import {z} from 'zod';
+import { z } from 'zod';
+
+export const settingsSchema = z.object({
+  name: z.optional(z.string()),
+})
+
+export type SettingsSchemaType = z.infer<typeof settingsSchema>;
 
 export const LoginSchema = z.object({
   email: z.string().email({
