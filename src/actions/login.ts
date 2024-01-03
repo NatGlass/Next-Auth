83735eabb62c9d022c159bd3494885a147afd3c0
Visibyte <nathanglass26@gmail.com>
2024-01-03 16:11:12 +0000

    Refactor login function to support two-factor authentication

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,12 +1,15 @@
 'use server';
 
-import {getUserByEmail} from '@/data/user';
-import {sendVerificationEmail} from '@/lib/email';
-import {generateVerificationToken} from '@/lib/tokens';
-import {LoginSchema, LoginSchemaType} from '@/schemas';
-import {AuthError} from 'next-auth';
-import {signIn} from '../../auth';
-import {DEFAULT_LOGIN_REDIRECT} from '../../routes';
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
+import { getUserByEmail } from '@/data/user';
+import { sendTwoFactorEmail, sendVerificationEmail } from '@/lib/email';
+import { generateTwoFactorToken, generateVerificationToken } from '@/lib/tokens';
+import { LoginSchema, LoginSchemaType } from '@/schemas';
+import { AuthError } from 'next-auth';
+import prisma from '@/lib/db';
+import { getTwoFactorConfirmationByUserId } from '@/data/two-factor-confirmation';
+import { signIn } from '../../auth';
+import { DEFAULT_LOGIN_REDIRECT } from '../../routes';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function login(values: LoginSchemaType) {
@@ -16,7 +19,7 @@ export async function login(values: LoginSchemaType) {
     return {error: 'Invalid credentials'};
   }
 
-  const {email, password} = validatedValues.data;
+  const {email, password, code} = validatedValues.data;
 
   const existingUser = await getUserByEmail(email);
 
@@ -30,6 +33,56 @@ export async function login(values: LoginSchemaType) {
     await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
     return {success: 'Verification email sent'};
+  }
+
+  if (existingUser.isTwoFactorEnabled && existingUser.email) {
+    if (code) {
+      const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+
+      if (!twoFactorToken) {
+        return { error: 'Invalid code' };
+      }
+        
+        if (twoFactorToken?.token !== code) {
+          return { error: 'Invalid code' };
+      }
+
+      const hasExpired = new Date(twoFactorToken.expires) < new Date();
+
+      if (hasExpired) {
+        return { error: 'Code has expired' };
+      }
+
+      await prisma.twoFactorToken.delete({
+        where: {
+          id: twoFactorToken.id,
+        },
+      });
+
+      const existingConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
+      
+      if (existingConfirmation) {
+        await prisma.twoFactorConfirmation.delete({
+          where: {
+            id: existingConfirmation.id,
+          },
+        });
+      }
+
+
+      await prisma.twoFactorConfirmation.create({
+        data: {
+          userId: existingUser.id,
+        }
+      })
+
+    } else {
+      const twoFactorToken = await generateTwoFactorToken(email);
+
+      await sendTwoFactorEmail(twoFactorToken.email, twoFactorToken.token);
+
+      return {twoFactor: true};
+    }
   }
 
   try {
