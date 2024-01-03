eed7d28f99481fd38573a6e4b792b6ea71625ddb
Visibyte <nathanglass26@gmail.com>
2024-01-03 13:47:08 +0000

    Add function to generate two-factor authentication token

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,7 +1,34 @@
 import {getPasswordResetTokenByEmail} from '@/data/password-reset-token';
 import {getVerificationTokenByEmail} from '@/data/verification-token';
 import {v4 as uuidv4} from 'uuid';
+import crypto from "crypto"
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
 import prisma from './db';
+
+export async function generateTwoFactorToken(email: string) {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 3600 * 1000); // 1 hour from now
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await prisma.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const twoFactorToken = await prisma.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
+}
 
 export async function generatePasswordResetToken(email: string) {
   const token = uuidv4();
