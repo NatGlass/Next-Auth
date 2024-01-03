ca3462c6bbae5dcbbc6139a28590a90746985d79
Visibyte <nathanglass26@gmail.com>
2024-01-03 13:46:52 +0000

    Add two-factor confirmation data retrieval function

---
+++ src/data/two-factor-confirmation.ts
@@ -0,0 +1,13 @@
+import prisma from '@/lib/db';
+
+export async function getTwoFactorConfirmationByUserId(userId: string) {
+  try {
+    const twoFactorConfirmation = await prisma.twoFactorConfirmation.findUnique({
+      where: {userId},
+    });
+
+    return twoFactorConfirmation;
+  } catch (error) {
+    return null;
+  }
+}
